d211464af1c926655c8dd35fa7569ee6958967d9
Pietari Kaskela <eXeP@users.noreply.github.com>
2016-06-17 18:21:20 +0300

    Update basic.cpp

--- numtheory/basic.cpp
+++ numtheory/basic.cpp
@@ -7,3 +7,15 @@ i64 gcd(i64 u, i64 v) {
   }
   return u < 0 ? -u : u; /* abs(u) */
 }
+
+i64 powmod(i64 b, i64 e, i64 m){
+	b%=m;
+	i64 re = 1LL;
+	while(e){
+		if(e&1)
+			re = (re*b)%m;
+		b = (b*b)%m;
+		e>>=1;
+	}
+	return re;
+}
